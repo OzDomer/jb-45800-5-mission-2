9e0b642a4dcac078c1e26124d6a795d9242128d0
Oz Domer <ozdomer7@gmail.com>
2026-04-14 12:17:46 +0300

    more formatting

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -3,7 +3,7 @@ import "./AboutPage.css"
 export default function AboutPage() {
         return (
         <div className="AboutPage">
-            <h2>About</h2>
+            <h1>About</h1>
             <p>This app allows you to search for current weather conditions in cities across Israel. Select a city from the list to see live temperature, wind speed, and weather conditions.</p>
             <p>not every city has a match in the api we deal with it by taking its piba key (region) if one exist and show the region weather some cities cant be handled with the api so and error page shows up</p>
             <h3>Developer</h3>
